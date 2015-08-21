1540c279e52b84c7dea11c5f575f94279a59e997
trainwizard <sam@sams-MacBook-Pro.local>
2015-08-21 10:44:12 -0500

    try 2

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("https://www.theknot.com/us/katelyn-irons-and-samuel-bolds-oct-2015"); ?>
+<?php
+header("Location: https://www.theknot.com/us/katelyn-irons-and-samuel-bolds-oct-2015");
+exit;
+?>
+
+/*<?php include_once("https://www.theknot.com/us/katelyn-irons-and-samuel-bolds-oct-2015"); ?>*/
